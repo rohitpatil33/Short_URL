4b765a4a6494dc190fb0cec876cc06d13d624004
rohitpatil33 <rohitpatil8226@gmail.com>
2025-03-21 20:54:02 +0530

    Updated models

--- models/url.js
+++ models/url.js
@@ -1,12 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-// Define schema for storing URLs
 const urlSchema = new mongoose.Schema({
     shortId: { type: String, required: true, unique: true },
     redirectURL: { type: String, required: true },
-    visitHistory: { type: Number, default: 0 }
-}, { timestamps: true });
+    visitHistory: [{ timestamp: { type: Number } }] // Change here
+});
 
-const URL = mongoose.model("URL", urlSchema);
-
-module.exports = URL;
+module.exports = mongoose.model('URL', urlSchema);
